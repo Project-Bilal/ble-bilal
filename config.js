e94612aa64d45d224be483487afe38dde24f8648
rafikee <rafik.vcu@gmail.com>
2024-10-26 23:08:51 -0700

    updates

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+
+export const SERVICE_UUID = "2b45e4e0-af38-4c4a-a4dc-4399a03a7b38";
+export const CHARACTERISTIC_UUID = "97d91c3e-1122-48b8-8b6f-8ffb2daa2bda";
+
+// You can add more configuration values here
